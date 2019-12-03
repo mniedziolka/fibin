f3905173429d0ed9e451b59b3d61b209db34159c
niedziol <michalekniedziolka@gmail.com>
2019-12-03 23:55:07 +0100

    Find in list

--- src/fibin.h
+++ src/fibin.h
@@ -1,12 +1,33 @@
-//
-// Created by chedat on 03.12.2019.
-//
-
 #include <type_traits>
 
-#ifndef _FIBIN_H
-#define _FIBIN_H
+#ifndef __FIBIN_H__
+#define __FIBIN_H__
 
+// Empty list.
+struct Nil;
+
+// Head <Var, Value> + Tail.
+template <unsigned Var, typename Value, typename Tail>
+struct Cons;
+
+template <typename List, unsigned Var>
+struct FindInList {};
+
+// Find in empty list.
+template <unsigned Var>
+struct FindInList<Nil, Var> {};
+
+// Searched element is in the current head.
+template <unsigned CurrVar, typename CurrValue, typename Tail>
+struct FindInList<Cons<CurrVar, CurrValue, Tail>, CurrVar> {
+    using value = CurrValue;
+};
+
+// Searched element is not in the current head but can be in tail.
+template <unsigned CurrVar, typename CurrValue, typename Tail, unsigned SearchedVar>
+struct FindInList<Cons<CurrVar, CurrValue, Tail>, SearchedVar> {
+    using value = typename FindInList<Tail, SearchedVar>::value;
+};
 
 template <typename Condition, typename Then, typename Else> class If;
 //true branch
@@ -35,4 +56,4 @@ template <typename ValueType> class Fibin {
 };
 
 
-#endif _FIBIN_H
+#endif // __FIBIN_H__
