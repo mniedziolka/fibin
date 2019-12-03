86f89139de100340ce88e9cbf849f6b6d44b9c42
Tomasz Cheda <chedatomasz@gmail.com>
2019-12-03 06:29:10 +0100

    stub

--- src/fibin.h
+++ src/fibin.h
@@ -1,6 +1,38 @@
-#ifndef __FIBIN_H__
-#define __FIBIN_H__
+//
+// Created by chedat on 03.12.2019.
+//
+
+#include <type_traits>
+
+#ifndef _FIBIN_H
+#define _FIBIN_H
+
+
+template <typename Condition, typename Then, typename Else> class If;
+//true branch
+template <typename Then, typename Else> class If <std::true_type, Then, Else>{
+    typedef double result;
+};
+
+//false branch
+template <typename Then, typename Else> class If <std::false_type, Then, Else>{
+    typedef int result;
+};
 
 
 
-#endif // __FIBIN_H__
+
+template <typename ValueType> class Fibin {
+
+    template <typename Expr>
+    static typename std::enable_if<std::is_integral<ValueType>::value, ValueType>::type eval(){
+
+    }
+    template <typename Expr>
+    static typename std::enable_if<!std::is_integral<ValueType>::value, void>::type eval(){
+
+    }
+};
+
+
+#endif _FIBIN_H
